961beb5713edd5f4dffa04aed09cd151c0de8468
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-10 12:05:39 +0100

    add list comprehension-convert integers to string

--- Starting_with_Numpy.py
+++ Starting_with_Numpy.py
@@ -7,3 +7,7 @@ print(np.__version__)
 ##create a list comprising numbers from 0 to 9
 L = list(range(10))
 print(L)
+
+#converting integers to string - this style of handling lists is known as list comprehension.
+S= [str(c) for c in L]
+print(S)

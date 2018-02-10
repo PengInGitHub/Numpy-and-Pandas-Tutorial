587dc80d571d6508b8e2c403607590066bde57c8
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-10 16:27:12 +0100

    add data.info()

--- Pandas.py
+++ Pandas.py
@@ -4,4 +4,10 @@ data = pd.DataFrame({'Country': ['Russia','Colombia','Chile','Equador','Nigeria'
                     'Rank':[121,40,100,130,11]})
 
 #quick analysis of data
-data.describe(include = 'all')
+#describe() method computes summary statistics of integer / double variables
+data.describe()
+
+#get the complete information about the data set
+data.info()
+
+data = pd.DataFrame({'group':['a', 'a', 'a', 'b','b', 'b', 'c', 'c','c'],'ounces':[4, 3, 12, 6, 7.5, 8, 3, 5, 6]})

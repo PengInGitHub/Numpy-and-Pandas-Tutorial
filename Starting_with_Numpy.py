ae73c8b6a56a991854a62e46624d8b4b0df6c919
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-10 12:14:32 +0100

    create matrix, np.ones((3,5),dtype='int')

--- Starting_with_Numpy.py
+++ Starting_with_Numpy.py
@@ -1,19 +1,27 @@
 import numpy as np
 
-##load the library and check its version
 
+#Check Version
+##load the library and check its version
 print(np.__version__)
 
+
+#Create List 
 ##create a list comprising numbers from 0 to 9
 L = list(range(10))
 print(L)
 
+
+#List Comprehension
 #converting integers to string - this style of handling lists is known as list comprehension.
 #List comprehension offers a versatile way to handle list manipulations tasks easily.
 S= [str(c) for c in L]
 print(S)
 print([type(i) for i in L])
 
+
 #Creating Arrays
 print(np.zeros(10,dtype='int'))
+#creating a 3 row x 5 column matrix
+print(np.ones((3,5),dtype='int'))
 

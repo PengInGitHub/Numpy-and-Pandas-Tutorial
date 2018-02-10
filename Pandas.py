f314ead98b25294bdc52202b92b92f9cf1b8cafc
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-10 16:45:11 +0100

    remove duplicated rows based on matching row values across all columns

--- Pandas.py
+++ Pandas.py
@@ -15,3 +15,9 @@ data = pd.DataFrame({'group':['a', 'a', 'a', 'b','b', 'b', 'c', 'c','c'],'ounces
 
 #sort the data frame by ounces
 data.sort_values(by='ounces',ascending=True,inplace=False)
+
+#get ride of duplicate rows
+#remove duplicates based on matching row values across all columns.
+data = pd.DataFrame({'k1':['one']*3 + ['two']*4, 'k2':[3,2,1,3,3,4,4]})
+data.sort_values(by='k2')
+data.drop_duplicates()

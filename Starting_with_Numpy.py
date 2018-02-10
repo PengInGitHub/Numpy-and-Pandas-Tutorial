4c371e67433af94488a9424af76208fa532891ca
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-10 12:10:46 +0100

    create arrary: np.zeros(10,dtype='int')

--- Starting_with_Numpy.py
+++ Starting_with_Numpy.py
@@ -9,5 +9,11 @@ L = list(range(10))
 print(L)
 
 #converting integers to string - this style of handling lists is known as list comprehension.
+#List comprehension offers a versatile way to handle list manipulations tasks easily.
 S= [str(c) for c in L]
 print(S)
+print([type(i) for i in L])
+
+#Creating Arrays
+print(np.zeros(10,dtype='int'))
+

0f92e4e5f0ab10fbc673eb5deeed6846925946fc
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 17:19:45 +0100

    predict

--- RandomForest.py
+++ RandomForest.py
@@ -12,3 +12,5 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=
 clf = RandomForestClassifier(n_estimators=500,max_depth=6)
 clf.fit(X_train,y_train)
 
+#predict
+prediction=clf.predict(X_test)

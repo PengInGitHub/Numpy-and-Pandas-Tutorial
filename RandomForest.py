ee34e85e19ed1a607135e8fad68e544dd1b74139
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 17:19:17 +0100

    train RF

--- RandomForest.py
+++ RandomForest.py
@@ -2,3 +2,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import cross_val_score
 from sklearn.metrics import accuracy_score
+
+y=train['target']
+del train['target']
+X=train
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=1,stratify=y)
+
+#train the RF classifier
+clf = RandomForestClassifier(n_estimators=500,max_depth=6)
+clf.fit(X_train,y_train)
+

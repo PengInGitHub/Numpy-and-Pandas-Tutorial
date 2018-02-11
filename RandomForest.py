3a4318d5a22b0e6f306b7477e4c32762c241cfaf
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 16:30:17 +0100

    init RandomForest.py

---
+++ RandomForest.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import accuracy_score
